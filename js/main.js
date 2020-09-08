c27277ea7558217195d003666718ba682698c318
Bohdan <52783471+Bohdan-pidkovych@users.noreply.github.com>
2020-09-08 10:37:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.info__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="Предыдущий слайд"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="Следующий слайд"></button>',
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true
+  });
+  $('.trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="Предыдущий слайд"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="Следующий слайд"></button>',
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true
+  });
+});
